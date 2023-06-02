7d81e3c7f3ad7e981097cab28a24728109e05be3
alyliann <92405765+alyliann@users.noreply.github.com>
2023-06-02 18:48:02 -0400

    Removing Duplicates

---
+++ Removing-Duplicates.py
@@ -0,0 +1,19 @@
+#Remove duplicates from sorted array
+
+def remove_duplicates(arr):
+  if (len(arr) < 2):
+    return arr
+  
+  pointer_1 = 0
+  pointer_2 = 1
+  while (pointer_2 != len(arr)):
+    if (arr[pointer_1] == arr[pointer_2]): #duplicate found
+      arr.remove(arr[pointer_1])
+    else:
+      pointer_1 += 1
+      pointer_2 += 1
+  return arr 
+  
+# change the values below to test different cases
+test_list = [2]
+print(remove_duplicates(test_list))
