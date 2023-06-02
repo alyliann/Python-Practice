4f0059f561b2caf90efcfe88853db0e3ad18a732
Alysa Vega <92405765+alyliann@users.noreply.github.com>
2023-06-02 18:28:50 -0400

    Lists Exercise 1

---
+++ Lists-Exercise1.py
@@ -0,0 +1,15 @@
+#Write a program that takes a list of integers called numbers and replaces each element greater than 10 with a '*'. Print the new version of numbers.
+
+# FREEZE CODE BEGINS
+import sys
+
+numbers = sys.argv [1:]
+for i in range (len (numbers)):
+    numbers[i] = int(numbers[i])
+# FREEZE CODE ENDS
+
+for i in range(len(numbers)):
+    if numbers[i] > 10:
+        numbers[i] = '*'
+            
+print (numbers)
