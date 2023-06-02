aa61ee9cd2feeaec0fc13a9acf0e43602f72a28c
alyliann <92405765+alyliann@users.noreply.github.com>
2023-06-02 18:33:14 -0400

    Functions and Recursion Exercise 3

---
+++ FunctionsRecursion-Exercise3.py
@@ -0,0 +1,9 @@
+#Write a recursive function called bunny_ears that takes the number of bunnies (an integer) as a parameter. Return the number of bunny ears (2 per bunny). Do not use multiplication; instead use addition.
+
+def bunny_ears(num):
+  if num == 0:
+    return 0
+  else:
+    return 2 + bunny_ears(num-1)
+
+print(bunny_ears(0))
