d256807f1f7c8e8867d83fa237ba34156e128092
alyliann <92405765+alyliann@users.noreply.github.com>
2023-06-02 18:34:18 -0400

    Functions and Recursion Exercise 5

---
+++ FunctionsRecursion-Exercise5.py
@@ -0,0 +1,10 @@
+#Write a recursive function called get_max that takes a list of numbers as a parameter. Return the largest number in the list.
+
+def get_max(nums):
+  if len(nums) == 1:
+    return nums[0]
+  else:
+    nums.remove(min(nums[len(nums) - 1], nums[len(nums) - 2]))
+    return get_max(nums)
+
+print(get_max([1, 2, 3, 4, 5]))
