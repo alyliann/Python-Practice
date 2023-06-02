8f041fa450abebd60029b15981e4ccd34c997335
alyliann <92405765+alyliann@users.noreply.github.com>
2023-06-02 18:44:59 -0400

    Linear Search

---
+++ Linear-Search.py
@@ -0,0 +1,13 @@
+#Implement Linear Search
+
+def search(search, find):
+  for i in range(len(search)):
+    if search[i] == find:
+      return i
+  return -1
+
+# FREEZE CODE BEGINS
+nums = list(map(int, input("Enter the list of integers separated by space ").strip().split()))
+find = int(input("Enter the integer to search for: "))
+print(search(nums, find))
+# FREEZE CODE ENDS
