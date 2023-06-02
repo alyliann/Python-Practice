6b98ab730723c0bb45ce09466f8577b7adb6aa02
alyliann <92405765+alyliann@users.noreply.github.com>
2023-06-02 18:33:52 -0400

    Functions and Recursion Exercise 4

---
+++ FunctionsRecursion-Exercise4.py
@@ -0,0 +1,10 @@
+#Write a recursive function called reverse_string that takes a string as a parameter. Return the string in reverse order. Hint, the slice operator will be helpful when solving this problem.
+
+def reverse_string(str):
+  if len(str) == 0:
+    return ''
+  else:
+    slice_str = slice(len(str) - 1)
+    return str[len(str) - 1] + reverse_string(str[slice_str])
+
+print(reverse_string("house"))
