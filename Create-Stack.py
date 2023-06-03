bfd114207b7963bf72b242cdff53fd45216194bc
alyliann <92405765+alyliann@users.noreply.github.com>
2023-06-03 09:29:56 -0400

    Create Stack

---
+++ Create-Stack.py
@@ -0,0 +1,66 @@
+# Fill in the following class DIYStack to create a fully functional stack.
+
+# You are given the below Node class to assist in your implementation.
+class Node:
+  def __init__(self, val):
+    self.value = val
+    self.next = None
+    self.prev = None
+
+class DIYStack:
+  
+  top_node = None
+  
+  def __init__(self):
+    top_node = None
+    
+  def push(self, value):
+    new_node = Node(value)
+    if isinstance(value, Node) == True:
+      new_node = value
+    
+    if self.top_node == None:
+      self.top_node = new_node
+    else:
+      new_node.prev = self.top_node
+      self.top_node.next = new_node
+      self.top_node = new_node
+
+      
+  def pop(self):
+    if self.top_node == None:
+      return None
+    else: # Remove & return the next element on the stack
+      return_node = self.top_node
+      self.top_node = self.top_node.prev
+      return return_node.value
+
+  
+  def peek(self):
+    if self.top_node == None:
+      return None
+    else: # Return the value of the next element to be popped
+      return self.top_node.value
+
+
+  def allNodesString(self):
+    curr_node = self.top_node
+    while (curr_node != None):
+      print(curr_node.value)
+      curr_node = curr_node.prev
+  
+
+# Instantiate + test your code here
+my_stack = DIYStack()
+print(my_stack)
+
+my_stack.push(1)
+my_stack.push(2)
+my_stack.allNodesString()
+
+new_node = Node(3)
+my_stack.push(new_node)
+print(my_stack.peek())
+
+my_stack.pop()
+my_stack.allNodesString()
