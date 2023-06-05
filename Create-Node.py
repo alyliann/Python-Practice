2406288a945ea8dd5dbedc9b44b865233a152976
alyliann <92405765+alyliann@users.noreply.github.com>
2023-06-05 14:08:52 -0400

    Create Node

---
+++ Create-Node.py
@@ -0,0 +1,13 @@
+# Write a program in the file to the left that defines a linked list node that holds a name. Your program should do the following:
+# Define a Node class
+# Define a constructor that accepts a name and next parameters in that order.
+# Create a linked list of one node that holds the name "Maria".
+# Print the name of the node.
+
+class Node:
+  def __init__(self, node_name, next_node):
+    self.name = node_name
+    self.next = next_node
+
+node = Node("Maria", None)
+print(node.name)
