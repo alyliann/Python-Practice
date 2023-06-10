4453204585a6055c70a989864d13f0ae9dc4d693
alyliann <92405765+alyliann@users.noreply.github.com>
2023-06-10 14:10:10 -0400

    Permutations

--- Permutations.py
+++ Permutations.py
@@ -3,3 +3,20 @@
 # Input: nums = [0,1]
 # Output: [[0,1],[1,0]]
 
+def permutations(nums, index=0, current=[]):
+  if len(nums) < 2:
+    return [nums]
+  if index == len(nums):
+    return current
+  
+  for i in range(len(nums)):
+    if i == index:
+      continue
+    temp = []
+    temp.append(nums[index])
+    temp.append(nums[i])
+    current.append(temp)
+
+  return permutations(nums, index+1, current)
+
+print(permutations([1,2]))
